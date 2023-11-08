5591cd512992db60ae8826c231596cb9d924fe1d
Alejandro Alvarez <alejandro_alvarez@outlook.com>
2023-11-08 00:25:10 -0300

    cambios 7del11b

--- app_musicos/urls.py
+++ app_musicos/urls.py
@@ -3,7 +3,12 @@ from app_musicos.views import *
 
 urlpatterns = [
     path("instrumentos/", listar_instrumentos(), name="lista_instrumentos"),
+    path("ingresar-instrumento/", listar_instrumentos(), name="ingresar_instrumento"),
+    path("buscar-instrumento/", listar_instrumentos(), name="buscar_instrumento"),
     path("musicos/", listar_musicos(), name="lista_musicos"),
+    path("ingresar-musico/", listar_musicos(), name="ingresar_musico"),
+    path("buscar-musico/", listar_musicos(), name="buscar_musico"),
+    path("ver-nota/", buscar_nota(), name="ver_nota"),
     path("crear-nota/", crear_nota(), name="crear_nota"),
     path("buscar-nota/", buscar_nota(), name="buscar_nota"),
 ]
--- proyecto/urls.py
+++ proyecto/urls.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
-from proyecto.views import saludar, saludar_con_html
-from proyecto.views import musicos
+from proyecto.views import saludar_con_html
+from app_musicos.views import listar_musicos
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("saludo/", saludar),
-    path("conocer/", musicos),
-    path("inicio/", saludar_con_html),
+    path("musicos/", include("app_musicos.urls")),
+    #path("inicio/", saludar_con_html),
+    path("", saludar_con_html, name="inicio"),
 ]
